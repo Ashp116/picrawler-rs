a54bd4f9fba168bcfddb11245b41cd1ce449c178
Ashp116 <ashp116@outlook.com>
2026-06-18 20:54:25 -0400

    UPDATE: Small wait before resetting mcu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{thread, time::Duration};
+
 mod device;
 mod utils;
 
@@ -5,6 +7,6 @@ fn main() {
     println!("Hello, world, I am picrawler!");
     device::reset_mcu();
     println!("Reset MCU done!");
-
+    thread::sleep(Duration::from_millis(1));
     println!("{}v Battery voltage", device::get_battery_voltage().unwrap());
 }
